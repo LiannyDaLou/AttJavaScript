6b4c53f44923b58c6a20ac1941e2de9324a2c320
Lianny <71676009+LiannyDaLou@users.noreply.github.com>
2024-04-23 08:20:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,49 +3,40 @@ function showCode(questionNumber) {
     if (code.style.display === 'none') {
         code.style.display = 'block';
     } else {
-        code.style.display = 'none';
-    }
-}
+        code.style.display = 'none';}}
 
 function calculateSum() {
-    var num1 = parseFloat(document.getElementById('num1').value);
-    var num2 = parseFloat(document.getElementById('num2').value);
+        var num1 = parseFloat(document.getElementById('num1').value);
+        var num2 = parseFloat(document.getElementById('num2').value);
 
     if (isNaN(num1) || isNaN(num2)) {
-        document.getElementById('output2').innerText = 'Por favor, insira dois números válidos.';
+            document.getElementById('output2').innerText = 'Por favor, insira dois números válidos.';
     } else {
         var soma = num1 + num2;
-        document.getElementById('output2').innerText = 'A soma de ' + num1 + ' e ' + num2 + ' é: ' + soma;
-    }
-}
+            document.getElementById('output2').innerText = 'A soma de ' + num1 + ' e ' + num2 + ' é: ' + soma;}}
 
 
 function changeColorById() {
     var question = document.getElementById('alt_id').querySelector('h3');
     var selectedColor = getRandomColor();
     question.style.transition = 'color 0.5s ease';
-    question.style.color = selectedColor;
-}
+    question.style.color = selectedColor;}
 
 function changeColorByName() {
     var question = document.getElementsByName('alt_name')[0].querySelector('h3');
     var selectedColor = getRandomColor();
     question.style.transition = 'color 0.5s ease';
-    question.style.color = selectedColor;
-}
+    question.style.color = selectedColor;}
 
 function changeColorByClass() {
     var question = document.querySelector('.alt_class').querySelector('h3');
     var selectedColor = getRandomColor();
     question.style.transition = 'color 0.5s ease';
-    question.style.color = selectedColor;
-}
+    question.style.color = selectedColor;}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
+        color += letters[Math.floor(Math.random() * 16)];}
+    return color;}
